b62748222ed88629588dd4d022f14fc58f9e4255
D Brox <65723952+D-Brox@users.noreply.github.com>
2021-02-10 14:41:44 -0300

    Update Modal.jsx

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -4,6 +4,7 @@ const { Modal } = require('powercord/components/modal')
 const { FormTitle, Text } = require('powercord/components')
 const { close: closeModal } = require('powercord/modal')
 const { Avatar } = getModule(['Avatar'], false)
+const { ListItem } = getModule(['ListItem'], false)
 const NotesHandler = new (require('../NotesHandler'))()
 
 class noteDisplay extends React.PureComponent {
@@ -16,18 +17,19 @@ class noteDisplay extends React.PureComponent {
   }
 
   render() {
-	const settings = NotesHandler.getNotes()
-	console.log(settings)
-	console.log(settings.length)
-	console.log(Object.keys(settings).length)
+	const notes = NotesHandler.getNotes()
+	console.log(notes)
+	console.log(notes.length)
+	console.log(Object.keys(notes).length)
 
 	const noteArray = []
 	const userId = []
 
-	for(let i = 0; i < Object.keys(settings).length; i++) {
-		let note = settings[Object.keys(settings)[i]]
-		userId.push(note['User ID'])
-		noteArray.push(<Avatar style={{'position' : 'absolute'}} src={note['Avatar URL']} size='SIZE_40'/>)
+	/* First option: A for loop just pushes a ton of stuff to an array to display later, so it's basically just displaying the note in plain text (looks awful) */
+	for(let i = 0; i < Object.keys(notes).length; i++) {
+		let note = notes[Object.keys(notes)[i]]
+		userId.push(note['User_ID'])
+        noteArray.push(<Avatar style={{'position' : 'absolute'}} src={note['Avatar_URL']} size='SIZE_40'/>)
 		noteArray.push(<span style={{
 		  'color' : 'white', 
 		  'position' : 'absolute', 
@@ -35,9 +37,13 @@ class noteDisplay extends React.PureComponent {
 		  'margin-top' : '23px'
 		}}>{note['Username']}</span>)
 		noteArray.push(<Text selectable={true} style={{'padding-left': '50px', 'position': 'absolute'}}>{note['Content']}</Text>)
-		noteArray.push(<br/>)
+        for(let j=0; j<(note['Content'].length/100+1);j++){
+        	noteArray.push(<br/>)
+        }
+        noteArray.push(<Text selectable={true} style={{'padding-left': '50px', 'position': 'absolute'}}>(message {note['Message_ID']})</Text>)
+    	noteArray.push(<br/>)
 	}
-
+    
 	return(	
 	  <Modal className='Notebook' size={Modal.Sizes.LARGE}>
 		<Modal.Header>
@@ -45,8 +51,8 @@ class noteDisplay extends React.PureComponent {
         		<Modal.CloseButton onClick={closeModal}/>
     		</Modal.Header>
 		<Modal.Content>
-			{noteArray}
-    	  	</Modal.Content>
+            		{noteArray}
+    		</Modal.Content>
 	  </Modal>
 	)
   }
