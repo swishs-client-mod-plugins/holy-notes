09679430fd44302c2897264a54b6c71b2e157c70
D Brox <65723952+D-Brox@users.noreply.github.com>
2021-02-10 13:01:48 -0300

    Update Modal.jsx

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -24,7 +24,6 @@ class noteDisplay extends React.PureComponent {
 	const noteArray = []
 	const userId = []
 
-	/* First option: A for loop just pushes a ton of stuff to an array to display later, so it's basically just displaying the note in plain text (looks awful) */
 	for(let i = 0; i < Object.keys(settings).length; i++) {
 		let note = settings[Object.keys(settings)[i]]
 		userId.push(note['User ID'])
@@ -38,29 +37,16 @@ class noteDisplay extends React.PureComponent {
 		noteArray.push(<Text selectable={true} style={{'padding-left': '50px', 'position': 'absolute'}}>{note['Content']}</Text>)
 		noteArray.push(<br/>)
 	}
-	
-	/* 2nd option: My shitty attempt at making a simulated channel and just pushing message data to that channel (not functional) */
-	const { default: Channel } = getModule([ 'isPrivate' ], false)
-	const fakeChannel = new Channel({ id: '6666' })
-
-	const ChannelMessage = getModuleByDisplayName('ChannelMessage', false)
-	// const channel = new fakeChannel()
-	// channel.push({settings})
 
 	return(	
 	  <Modal className='Notebook' size={Modal.Sizes.LARGE}>
-		
 		<Modal.Header>
-        <FormTitle tag='h3'>Notebook</FormTitle>
-        <Modal.CloseButton onClick={closeModal}/>
-    	</Modal.Header>
-		
+        		<FormTitle tag='h3'>Notebook</FormTitle>
+        		<Modal.CloseButton onClick={closeModal}/>
+    		</Modal.Header>
 		<Modal.Content>
 			{noteArray}
-			<Card className='smartTypers-preview'>
-      			<fakeChannel/>
-    		</Card>
-    	</Modal.Content>
+    	  	</Modal.Content>
 	  </Modal>
 	)
   }
